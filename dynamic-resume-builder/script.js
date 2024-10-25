f295996a7d0edfb0d23add6f86dc67cecb25ada5
Babar Mehmood <babarzaidi1122@gmail.com>
2024-10-25 15:20:46 +0500

    dynamic-resume-build

---
+++ dynamic-resume-builder/script.js
@@ -0,0 +1,32 @@
+// listing element
+var _a;
+(_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    event.preventDefault();
+    //type assertion
+    var nameElement = document.getElementById('name');
+    var emailElement = document.getElementById('email');
+    var phoneElement = document.getElementById('phone');
+    var educationElement = document.getElementById('education');
+    var experienceElement = document.getElementById('experience');
+    var skillsElement = document.getElementById('skills');
+    if (nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+        var name_1 = nameElement.value;
+        var email = emailElement.value;
+        var phone = phoneElement.value;
+        var education = educationElement.value;
+        var experience = experienceElement.value;
+        var skills = skillsElement.value;
+        //create resume output  
+        var resumeOutput = "\n    <h2>Resume</h2>\n    <p><strong>Name:</strong>".concat(name_1, " </p>\n    <p><strong>Email:</strong>").concat(email, " </p>\n    <p><strong>Phone</strong>").concat(phone, " </p>\n    \n   <h2>Education</h2>\n   <p>").concat(education, "</p>\n    \n  <h2>Experience</h2>\n   <p>").concat(experience, "</p>\n    \n  <h2>Skills</h2>\n   <p>").concat(skills, "</p>\n    ");
+        var resumeOutputElement = document.getElementById('resumeOutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+        }
+        else {
+            console.error('the resume output elements are missing');
+        }
+    }
+    else {
+        console.error('one or more output elements are missing');
+    }
+});
